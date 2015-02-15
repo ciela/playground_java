9d50fd81bd8f47d764911db5b08d2119a1be9e15
ciela <zektbach@gmail.com>
2015-02-15 23:09:52 +0900

    Create aws_sdk_route53

---
+++ aws_sdk_route53/src/main/java/GetDomainFromIP.java
@@ -0,0 +1,13 @@
+import com.amazonaws.services.route53.AmazonRoute53;
+import com.amazonaws.services.route53.AmazonRoute53Client;
+
+/**
+ * Created by ota_kazuhiro on 2015/02/15.
+ */
+public class GetDomainFromIP {
+
+    public static void main(String[] args) {
+        AmazonRoute53 route53 = new AmazonRoute53Client();
+    }
+
+}
